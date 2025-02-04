756a121d3520a50a61e29b92c84bb1dfa6025369
Miquel685 <miquelrosselloadrover@a.cifpperedesongall.org>
2025-02-04 20:09:24 +0100

    Clase aliminar producte per nom

---
+++ src/eliminarProdNombre.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class eliminarProdNombre {
+    public static String[] eliminarProdNombre(String[] inventario, String nombre) {
+        for (int i = 0; i < inventario.length; i++) {
+            if (inventario[i].equals(nombre)) {
+                int pos = Arrays.binarySearch(inventario, nombre);
+                String[] nuevoInventario = new String[inventario.length - 1];
+                System.arraycopy(inventario, 0, nuevoInventario, 0, pos);
+                System.arraycopy(inventario, pos + 1, nuevoInventario, pos, inventario.length - pos - 1);
+                inventario = nuevoInventario;
+            }
+        }
+        return inventario;
+    }
+}
