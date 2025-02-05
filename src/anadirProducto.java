bffd87d4d38ab3077958692771a5711eedd4521a
Miquel685 <miquelrosselloadrover@a.cifpperedesongall.org>
2025-02-05 19:55:12 +0100

    Ja sa fet la refactoritzacio i el control de erros

--- src/anadirProducto.java
+++ src/anadirProducto.java
@@ -5,9 +5,6 @@ public class anadirProducto {
     public static String[] anadirProductoinventario(String [] inventario, String respuesta) {
         inventario = Arrays.copyOf(inventario, inventario.length + 1);
         inventario[inventario.length-1] = respuesta;
-        System.out.println(Arrays.toString(inventario));
         return inventario;
-
     }
-
 }
--- src/modificarProducto.java
+++ src/modificarProducto.java
@@ -2,11 +2,13 @@ public class modificarProducto {
     public static String[] modificarProductoinventari(String[] inventario) {
         for (int i = 0; i < inventario.length; i++) {
             System.out.println("Que producto quieres modificar?");
-            String prodeliminar = Main.valor();
+            String prodmodificar = Main.valor();
             System.out.println("porque lo quieres sustituir: ");
             String nuevaVersionProducto = Main.valor();
-            if (inventario[i].equals(prodeliminar)) {
+            if (inventario[i].equals(prodmodificar)) {
                 inventario[i] = nuevaVersionProducto;
+            }else {
+                System.out.println("No se puede modificar el producto, porque mo se encuentra");
             }
         }
         return inventario;
