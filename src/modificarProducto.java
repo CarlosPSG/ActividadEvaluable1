b360a278a40f5db024a53b313158644fc51182e2
Miquel685 <miquelrosselloadrover@a.cifpperedesongall.org>
2025-02-06 10:57:59 +0100

    fallo en modificar producto

--- src/modificarProducto.java
+++ src/modificarProducto.java
@@ -1,11 +1,11 @@
 public class modificarProducto {
     public static String[] modificarProductoinventari(String[] inventario) {
+        System.out.println("Que producto quieres modificar?");
+        String prodmodificar = Main.valor();
         for (int i = 0; i < inventario.length; i++) {
-            System.out.println("Que producto quieres modificar?");
-            String prodmodificar = Main.valor();
-            System.out.println("porque lo quieres sustituir: ");
-            String nuevaVersionProducto = Main.valor();
             if (inventario[i].equals(prodmodificar)) {
+                System.out.println("porque lo quieres sustituir: ");
+                String nuevaVersionProducto = Main.valor();
                 inventario[i] = nuevaVersionProducto;
             }else {
                 System.out.println("No se puede modificar el producto, porque mo se encuentra");
